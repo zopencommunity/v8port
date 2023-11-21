344cdfc6ede6af126ad128b8bdba10018f816f08
Gaby Baghdadi <baghdadi@ca.ibm.com>
2023-11-21 17:58:49 -0500

    files from zopen generate

---
+++ README.md
@@ -0,0 +1,3 @@
+v8
+
+A port of Google V8 JavaScript engine to z/OS Open Tools project
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/v8port.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/v8port.git'), string(name: 'PORT_DESCRIPTION', value: 'A port of Google V8 JavaScript engine to z/OS Open Tools project' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
