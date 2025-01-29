f7efe3516f360c0de01977c3598d2472d9e6d2f0
John F. Davis <john.davis@broadcom.com>
2025-01-29 16:34:36 -0500

    add mod for real hardware

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -9,6 +9,6 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/zopencommunity/v8port.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/v8port.git'), string(name: 'PORT_DESCRIPTION', value: 'A port of Google V8 JavaScript engine to z/OS Open Tools project' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/v8port.git'), string(name: 'PORT_DESCRIPTION', value: 'A port of Google V8 JavaScript engine to z/OS Open Tools project' ), string(name: 'BUILD_LINE', value: 'DEV'), string(name: 'NODE_LABEL', value: "v3r1" ) ]
   }
 }
